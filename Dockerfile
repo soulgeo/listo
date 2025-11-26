0587856613e57016acb52610d7597dbfda113a96
George Soulantikas <soulgeo01@gmail.com>
2025-11-26 11:11:35 +0200

    Migration to PostgreSQL, docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
-FROM python:3.13-alpine
+FROM python:3.13.9-slim-bookworm
+
+ENV PYTHONWRITEBUTECODE=1 \
+PYTHONUNBUFFERED=1
 
 WORKDIR /app
+
+RUN apt-get update && apt-get install -y curl
 
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
 COPY . .
+
+EXPOSE 8000
 
 CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
--- listo/settings.py
+++ listo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,8 +80,12 @@ WSGI_APPLICATION = "listo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
     }
 }
 
